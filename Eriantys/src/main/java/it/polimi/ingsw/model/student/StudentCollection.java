e50aef09f7128a572f8e104c06825e7583f22dc6
FedeAlbe <federico.albertini@mail.polimi.it>
2022-03-24 02:35:23 +0100

    Finished StudentCollection Class

---
+++ Eriantys/src/main/java/it/polimi/ingsw/model/student/StudentCollection.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.model.student;
+
+
+import java.util.*;
+
+public class StudentCollection {
+
+
+
+    private final HashMap<Student, Integer>  students = new HashMap<>();
+
+    public HashMap<Student, Integer> getStudents() {
+        return students;
+    }
+    public int getCount(Student s) {
+       return students.get(s);
+    }
+
+    public int getTotalCount(){
+        int totalCount = 0;
+        for(Student i : Student.values()) {
+            totalCount += getCount(i);
+        }
+        return totalCount;
+    }
+
+    public void removeStudents(Student s, int count){
+        students.put(s, students.get(s) - count);
+    }
+
+    void addStudents(Student s, int count) {
+        students.put(s, count);
+    }
+
+    public Student pickRandom() {
+        Random random = new Random();
+        return Student.values()[random.nextInt(Student.values().length)];
+
+    }
+
+}
