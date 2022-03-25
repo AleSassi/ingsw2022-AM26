33626fdda0dd7bf9d80406fa56e3eb08fbb1b1e0
Alessandro Sassi <AleSassi@users.noreply.github.com>
2022-03-25 12:37:26 +0100

    Added a way to get a random Student

--- src/main/java/it/polimi/ingsw/model/student/Student.java
+++ src/main/java/it/polimi/ingsw/model/student/Student.java
@@ -1,20 +1,30 @@
 package it.polimi.ingsw.model.student;
 
 import it.polimi.ingsw.model.Professor;
+import java.util.*;
 
 public enum Student {
     YellowElf (Professor.YellowElf),
     BlueUnicorn (Professor.BlueUnicorn),
     GreenFrog (Professor.GreenFrog),
     RedDragon (Professor.RedDragon),
-    PinkFair(Professor.PinkFair);
+    PinkFair (Professor.PinkFair);
 
-    Professor associatedProfessor;
+    private final Professor associatedProfessor;
+
+    private static final Random randomizer = new Random();
+    private static final List<Student> values = Collections.unmodifiableList(Arrays.asList(values()));
 
     Student(Professor professor){
         associatedProfessor = professor;
     }
-    public Professor getAssociatedProfessor(){
+
+    public Professor getAssociatedProfessor() {
         return this.associatedProfessor;
+    }
+
+    public static Student getRandomStudent() {
+        int randIndex = randomizer.nextInt(0, values.size());
+        return values.get(randIndex);
     }
 }
