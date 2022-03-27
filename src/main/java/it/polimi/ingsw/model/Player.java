15b98f85c7e04522922b2421c0fe8c21aaae580d
Alessandro Sassi <AleSassi@users.noreply.github.com>
2022-03-27 13:50:35 +0200

    Added the "skeleton" structure of the Player and TableManager classes

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,4 +1,103 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.assistants.AssistantCard;
+import it.polimi.ingsw.model.assistants.Wizard;
+import it.polimi.ingsw.model.characters.*;
+import it.polimi.ingsw.model.student.*;
+import java.util.*;
+
 public class Player {
+
+    private final String nickname;
+
+    public Player(String nickname, Wizard wiz, Tower towerColor, int initialTowerCount) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public List<AssistantCard> getAvailableAssistantCards() {
+        return null;
+    }
+
+    public AssistantCard getLastPlayedAssistantCard() {
+        return null;
+    }
+
+    public List<Professor> getControlledProfessors() {
+        return null;
+    }
+
+    public int getCountAtTable(Student s) {
+        return 0;
+    }
+
+    public CharacterCard getActiveCharacterCard() {
+        return null;
+    }
+
+    public void playAssistantCardAtIndex(int cardIndex) {
+
+    }
+
+    public void addStudentToEntrance(Student s) {
+
+    }
+    public void removeStudentFromEntrance(Student s) {
+
+    }
+
+    /**
+     * Returns the number of students with the same type as S currently in the Dining Room
+     */
+    public int placeStudentAtTableAndGetCoin(Student s) {
+        return 0;
+    }
+
+    public void removeStudentFromTable(Student s) {
+
+    }
+
+    public void addProfessor(Professor p) {
+
+    }
+
+    public void removeProfessor(Professor p) {
+
+    }
+
+    public Tower getTowerType() {
+        return Tower.Black;
+    }
+
+    public void gainTower() {
+
+    }
+
+    public Tower pickAndRemoveTower() {
+        return Tower.Black;
+    }
+
+    public void addAllStudentsToEntrance(StudentCollection sc) {
+
+    }
+
+    public void playCharacterCard(CharacterCard card) {
+
+    }
+
+    public void deactivateCard() {
+
+    }
+
+    private boolean isWinner() {
+        return false;
+    }
+
+    public void notifyVictory() {
+
+    }
+
 }
---
+++ src/main/java/it/polimi/ingsw/model/TableManager.java
@@ -0,0 +1,60 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.student.*;
+
+public class TableManager {
+
+    public TableManager(int cloudTileCount) {
+
+    }
+
+    public int getNumberOfClouds() {
+        return 0;
+    }
+
+    public void getCurrentIsland() {
+
+    }
+
+    public StudentCollection pickStudentsFromBag(int count) {
+        return null;
+    }
+
+    public void putStudentInBag(Student s) {
+
+    }
+
+    public StudentCollection pickStudentsFromCloud(int cloudIdx) {
+        return null;
+    }
+
+    public void placeStudentOnCloud(Student s, int cloudIdx) {
+
+    }
+
+    public void placeStudentOnIsland(Student s, int islandIdx) {
+
+    }
+
+    public void moveMotherNature(int steps) {
+
+    }
+
+    public int getInfluenceOnCurrentIsland(Player p) {
+        return 0;
+    }
+
+    public void changeControlOfCurrentIsland(Player from, Player to) {
+
+    }
+
+    private void unifyCurrentIslandWithAdjacentIfPossible() {
+
+    }
+
+    private void checkAndNotifyMatchEnd() {
+
+    }
+
+}
