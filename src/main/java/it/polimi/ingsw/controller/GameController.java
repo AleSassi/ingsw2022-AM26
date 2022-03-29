03420aac1fb31b9d3c1dcc16dbcbf6c0b0fd9a97
Alessandro Sassi <AleSassi@users.noreply.github.com>
2022-03-29 22:05:34 +0200

    Fixed some errors due to wrong imports in some placeholder Controller classes Updated JUnit dependency

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.controller;
 import java.util.*;
-import java.*;
 
 public class GameController {
 
--- src/main/java/it/polimi/ingsw/controller/notifications/NotificationName.java
+++ src/main/java/it/polimi/ingsw/controller/notifications/NotificationName.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.controller.notifications;
 import java.util.*;
-import java.*;
 
 public enum NotificationName {
 }
